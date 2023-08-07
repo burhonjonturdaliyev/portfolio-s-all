7aead975a579e2168185c792882ad9f5a5a52b0b
Burkhonjon Turdialiev <burhonjonturdaliyev2003@gmail.com>
2023-08-07 16:39:49 +0500

    first commit

---
+++ Protfolio_Flutter_Website-master/lib/changes/links.dart
@@ -0,0 +1,26 @@
+// CV
+const String resume = 'https://instagram.com/burhonjon_turdaliyev_';
+
+// Github Link
+const String gitHub = 'https://github.com/burhonjonturdaliyev';
+// Github Link
+const String facebook = 'https://facebook.com/BurhonjonTurdaliyev';
+// Github Link
+const String twitter = 'https://instagram.com/burhonjon_turdaliyev_';
+// Github Link
+const String youtube = 'https://www.youtube.com/channel/BurhonjonTurdaliyev';
+// Github Link
+const String linkedin = 'https://instagram.com/burhonjon_turdaliyev_';
+// Github Link
+const String buymeacoffee = 'https://instagram.com/burhonjon_turdaliyev_';
+// Github Link
+const String instagram = 'https://instagram.com/burhonjon_turdaliyev_';
+const String profileImage =
+    "https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80";
+//
+// Github Link
+const String fiverr = 'https://instagram.com/burhonjon_turdaliyev_';
+// Github Link
+const String upwork = 'https://instagram.com/burhonjon_turdaliyev_';
+// Github Link
+const String whatsapp = 'https://instagram.com/burhonjon_turdaliyev_';
---
+++ Protfolio_Flutter_Website-master/lib/changes/strings.dart
@@ -0,0 +1,26 @@
+String hellotag = '''Hi there, Welcome to My Space  ''';
+
+String yourname = "I'm Burkhonjon Turdialiev,";
+// String hometitle2 = "A Mobile Application\nDeveloper";
+
+String animationtxt1 = " Mobile Application Developer";
+
+//
+String contactHeadding = "Let’s try my service now!";
+String contactSubHeadding =
+    "Let’s work together and make everything super cute and super useful.";
+//
+String miniDescription =
+    "I'm a third year student of Sharda University Uzbekistan. I have started to learn flutter 10 month before for only my self. Let's came to university, my major is Computer science and engineering";
+
+String servicesSubHeading =
+    "I'm a third year student of Sharda University Uzbekistan. I have started to learn flutter 10 month before for only my self. Let's came to university, my major is Computer science and engineering";
+//
+
+String protfolioSubHeading =
+    "I'm a third year student of Sharda University Uzbekistan. I have started to learn flutter 10 month before for only my self. Let's came to university, my major is Computer science and engineering";
+const String aboutMeHeadline =
+    'I\'m Burkhonjon Turdialiev, a Mobile App developer, Web developer and UI designer.';
+
+const String aboutMeDetail =
+    'I\'m a third year student of Sharda University Uzbekistan. I have started to learn flutter 10 month before for only my self. Let\'s came to university, my major is Computer science and engineering';
